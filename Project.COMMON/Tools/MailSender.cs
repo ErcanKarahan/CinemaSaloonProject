e6d0db0ba9c27d4b247fb2788aa245f9c0408f15
Ercan Karahan <ercankarahan@hotmail.de>
2021-02-27 20:50:15 +0300

    Email Aktivasyon kod gönderme işlemi Tamam.

--- Project.COMMON/Tools/MailSender.cs
+++ Project.COMMON/Tools/MailSender.cs
@@ -10,7 +10,7 @@ namespace Project.COMMON.Tools
     public static class MailSender
     {
       
-        public static void Send(string receiver, string password = "emreemretest", string body = "Deneme", string subject = "Test", string sender = "ercankarahan@hotmail.de")
+        public static void Send(string receiver, string password = "Ak17041971", string body = "Deneme", string subject = "Test", string sender = "ercankarahan@hotmail.de")
         {
 
             MailAddress senderEmail = new MailAddress(sender);
@@ -20,7 +20,7 @@ namespace Project.COMMON.Tools
 
             SmtpClient smtp = new SmtpClient
             {
-                Host = "smtp.gmail.com",
+                Host = "SMTP.office365.com",
                 Port = 587,
                 EnableSsl = true,
                 DeliveryMethod = SmtpDeliveryMethod.Network,
