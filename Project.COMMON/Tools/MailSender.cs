c6791026d4c3ecef5bc63e8ced11c0a3a59d2b8c
Ercan Karahan <ercankarahan@hotmail.de>
2021-02-27 19:57:18 +0300

    Mail Sender email adresi elklendi.

--- Project.COMMON/Tools/MailSender.cs
+++ Project.COMMON/Tools/MailSender.cs
@@ -10,7 +10,7 @@ namespace Project.COMMON.Tools
     public static class MailSender
     {
       
-        public static void Send(string receiver, string password = "emreemretest", string body = "Deneme", string subject = "Test", string sender = "emregorentest@gmail.com")
+        public static void Send(string receiver, string password = "emreemretest", string body = "Deneme", string subject = "Test", string sender = "ercankarahan@hotmail.de")
         {
 
             MailAddress senderEmail = new MailAddress(sender);
